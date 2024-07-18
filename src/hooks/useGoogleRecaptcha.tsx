20d7318fb9d4303de2fe1943ff6ba2c8d4105ac7
Google <opensource@google.com>
2024-07-18 14:09:26 +0700

    fix: fix optimze

--- src/hooks/useGoogleRecaptcha.tsx
+++ src/hooks/useGoogleRecaptcha.tsx
@@ -20,55 +20,48 @@ const useGoogleRecaptcha = (
   siteKey: string,
   action: string,
   options: { language?: string } = {}
-
 ) => {
   const [token, setToken] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
-  const { language = 'en' } = options; 
+  const { language = "en" } = options;
 
   useEffect(() => {
+    const scriptId = "recaptcha-script";
+
     const loadReCaptcha = () => {
-      window.grecaptcha.ready(() => {
-        window.grecaptcha
-          .execute(siteKey, { action })
-          .then((token) => setToken(token))
-          .catch((err) => setError(err));
-      });
+      if (window.grecaptcha) {
+        window.grecaptcha.ready(() => {
+          window.grecaptcha
+            .execute(siteKey, { action })
+            .then((token) => setToken(token))
+            .catch((err) => setError(err));
+        });
+      }
     };
 
-    const script = document.createElement("script");
-    script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}&hl=${language}`;
-    script.async = true;
-    script.defer = true;
-    script.onload = loadReCaptcha;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
+    if (!document.getElementById(scriptId)) {
+      const script = document.createElement("script");
+      script.id = scriptId;
+      script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}&hl=${language}`;
+      script.async = true;
+      script.defer = true;
+      script.onload = loadReCaptcha;
+      document.body.appendChild(script);
+    } else {
+      loadReCaptcha();
+    }
   }, [siteKey, action, language]);
 
   const fetchCaptcha = useCallback(() => {
-    const loadReCaptcha = () => {
+    if (window.grecaptcha) {
       window.grecaptcha.ready(() => {
         window.grecaptcha
           .execute(siteKey, { action })
           .then((token) => setToken(token))
           .catch((err) => setError(err));
       });
-    };
-
-    const script = document.createElement("script");
-    script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}&hl=${language}`;
-    script.async = true;
-    script.defer = true;
-    script.onload = loadReCaptcha;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [siteKey, action, language]);
+    }
+  }, [siteKey, action]);
 
   return { token, error, fetchCaptcha };
 };
